1ad194dd8ec0d231125cc617865362c40299df94
vdfrede <vdfrede@gmail.com>
2022-11-07 20:12:51 +0100

    Added example

--- Main.java
+++ Main.java
@@ -1,7 +1,9 @@
 import directory1.*;
+import thesislib.Diagram;
 public class Main {
     
     public static void main(String[] args) {
-    
+    Diagram d = new Diagram("Diagram", "diagram.plantuml", ClassE.class);
+    d.printDiagram();
     }
 }
